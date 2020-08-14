279c9efb5bdd5973c583202d1631f7e0c6845f2c
sraavcoder <61928159+sraavcoder@users.noreply.github.com>
2020-08-14 21:54:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+
+export default class App extends React.Component {
+  render() {
+    return <Home />;
+  }
+}
